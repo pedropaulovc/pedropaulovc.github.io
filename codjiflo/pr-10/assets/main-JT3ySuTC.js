189ec07042e80a837948486d26cd5488cfbb4855
pedropaulovc <pedropaulovc@users.noreply.github.com>
2025-12-25 23:28:44 +0000

    deploy: pedropaulovc/codjiflo@2b64b6b81929b3878e02660625012a489b01671d

--- codjiflo/pr-10/assets/main-JT3ySuTC.js
+++ codjiflo/pr-10/assets/main-JT3ySuTC.js
@@ -2,7 +2,7 @@ import { j as jsxRuntimeExports } from "./_virtual/jsx-runtime-CHpI9sbA.js";
 import { r as reactExports } from "./_virtual/index-TeEMZYt5.js";
 import { c as clientExports } from "./_virtual/client-DzxbJFbo.js";
 /* empty css                   */
-import App from "./App-erSVD0PI.js";
+import App from "./App-CMMiAZmx.js";
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found");
@@ -10,4 +10,4 @@ if (!rootElement) {
 clientExports.createRoot(rootElement).render(
   /* @__PURE__ */ jsxRuntimeExports.jsx(reactExports.StrictMode, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {}) })
 );
-//# sourceMappingURL=main-CG-tb0Z7.js.map
+//# sourceMappingURL=main-JT3ySuTC.js.map
